9101b71bbb983585f9433989e41ad4da83739b3c
John Källén <johnkal@gmail.com>
2024-12-09 16:08:58 +0100

    Year 2015 - day 5 - part 1

---
+++ 2015/05/Program.cs
@@ -0,0 +1,31 @@
+﻿var lines = File.ReadAllLines(args[0]);
+
+HashSet<string> badStrings = [
+    "ab", "cd", "pq", "xy"
+];
+int nice = lines.Where(l => IsNice(l, badStrings)).Count();
+Console.WriteLine($"Nice: {nice}");
+
+
+static bool IsNice(string s, HashSet<string> badStrings)
+{
+    int cVowels = 0;
+    int cDoubled = 0;
+    int cBad = 0;
+    for (int i = 0; i < s.Length; ++i)
+    {
+        if ("aeiou".Contains(s[i]))
+            ++cVowels;
+        if (i > 0)
+        {
+            if (s[i] == s[i - 1])
+                ++cDoubled;
+            foreach (var bad in badStrings)
+            {
+                if (s[i - 1] == bad[0] && s[i] == bad[1])
+                    ++cBad;
+            }
+        }
+    }
+    return cVowels >= 3 && cDoubled > 0 && cBad == 0;
+}
