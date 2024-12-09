150728acdd1b230d5e77d2dc11070b6907831308
John Källén <johnkal@gmail.com>
2024-12-09 15:40:19 +0100

    Year 2015 - Day 2 & 3

---
+++ 2015/02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+var dimensions = ReadPresentDimensions(args[0]);
+long totalPaper = dimensions.Select(d => PaperRequired(d)).Sum();
+long totalRibbon = dimensions.Select(d => RibbonRequired(d)).Sum();
+
+Console.WriteLine($"Total paper required: {totalPaper} sq ft");
+Console.WriteLine($"Total paper required: {totalRibbon} sq ft");
+
+static long PaperRequired(int[] dims)
+{
+    var l = dims[0];
+    var w = dims[1];
+    var h = dims[2];
+    var sides = new long[] {
+        l*w,
+        l*h,
+        w*h
+    };
+    var smallestSide = sides.Min();
+    return smallestSide + 2 * sides.Sum();
+}
+
+static long RibbonRequired(int[] dims)
+{
+    var l = dims[0];
+    var w = dims[1];
+    var h = dims[2];
+    var perimeters = new long[] {
+        2*(l+w),
+        2*(l+h),
+        2*(w+h),
+    };
+    var smallestPerimeter = perimeters.Min();
+    var bow = l*w*h;
+    return smallestPerimeter + bow;
+}
+
+static List<int[]> ReadPresentDimensions(string filename)
+{
+    var result = new List<int[]>();
+    using var rdr = File.OpenText(filename);
+    for (; ; )
+    {
+        string? line = rdr.ReadLine();
+        if (line is null)
+            return result;
+        var dims = line.Split('x').Select(x => int.Parse(x)).ToArray();
+        if (dims.Length != 3)
+            throw new InvalidDataException();
+        result.Add(dims);
+    }
+}
---
+++ 2015/03/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Numerics;
+using System.Security;
+
+var directions = ReadDirections(args[0]);
+var visited = Walk(directions);
+Console.WriteLine($"Houses visited: {visited.Count}");
+
+HashSet<Position>  Walk(string directions)
+{
+    var walkers = new[]
+    {
+        new Position(0, 0),
+        new Position(0, 0),
+    };
+    HashSet<Position> result = [ new(0, 0) ];
+    int iWalker = 0;
+    foreach (char ch in directions)
+    {
+        var pos = walkers[iWalker];
+        Vector vec = ch switch
+        {
+            '>' => new(1, 0) ,
+            'v' => new(0, 1) ,
+            '<' => new(-1, 0) ,
+            '^' => new(0, -1) ,
+            _ => throw new InvalidDataException($"Invalid direction {ch}.")
+        };
+        pos += vec;
+        result.Add(pos);
+        walkers[iWalker] = pos;
+        iWalker = 1 - iWalker;
+    }
+    return result;
+}
+
+static string ReadDirections(string filename)
+{
+    var lines = File.ReadAllLines(filename);
+    return lines[0];
+}
+
+public record struct Position(int X, int Y)
+{
+    public static Position operator +(Position pos, Vector vec)
+    {
+        return new(pos.X+vec.X, pos.Y+vec.Y);
+    }
+
+        public static Position operator -(Position pos, Vector vec)
+    {
+        return new(pos.X-vec.X, pos.Y-vec.Y);
+    }
+}
+
+public record struct Vector(int X, int Y);
