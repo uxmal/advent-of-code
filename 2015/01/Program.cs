97ad826b037b1a1c96cdee9230b3273067f4b277
John Källén <johnkal@gmail.com>
2024-12-09 14:31:17 +0100

    Year 2015 - day 1

---
+++ 2015/01/Program.cs
@@ -0,0 +1,17 @@
+﻿var instrs = File.ReadAllLines(args[0]).First();
+int floor = 0;
+int pos = 0;
+foreach (var c in instrs)
+{
+    switch (c)
+    {
+        case '(': ++floor; break;
+        case ')': --floor; break;
+        default: break;
+    }
+    ++pos;
+    if (floor == -1)
+        break;
+}
+Console.WriteLine($"Pos {pos}");
+Console.WriteLine($"Floor {floor}");
