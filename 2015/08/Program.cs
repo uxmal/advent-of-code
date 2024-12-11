fab879a90974da53eef68854118bee4e1ca6dcc3
John Källén <johnkal@gmail.com>
2024-12-11 20:42:44 +0100

    Year 2015 - day 8 - part 2

--- 2015/08/Program.cs
+++ 2015/08/Program.cs
@@ -12,18 +12,36 @@ static void interactive()
 {
     for (;;)
     {
+        Console.Out.Write("$ ");
         string? line = Console.ReadLine();
         if (line is null || line.Length == 0)
             break;
         var diff = ComputeSizeDifference(line);
-        Console.WriteLine($"Difference: {diff}");
+        var ediff = ComputeEncodedSizeDifference(line);
+        Console.WriteLine($"Difference:         {diff}");
+        Console.WriteLine($"Encoded difference: {ediff}");
     }
 }
 
 static void batch(string filename)
 {
     var sumDiff = File.ReadLines(filename).Sum(line => ComputeSizeDifference(line));
-    Console.WriteLine($"Sum of differences: {sumDiff}");
+    Console.WriteLine($"Part1: Sum of differences: {sumDiff}");
+    var sumDiffEncoded = File.ReadLines(filename).Sum(line => ComputeEncodedSizeDifference(line));
+    Console.WriteLine($"Part2: Sum of encoded differences: {sumDiffEncoded}");
+}
+
+static int ComputeEncodedSizeDifference(string line)
+{
+    int chars = 2;  // start and end quote.
+    for (int i = 0; i < line.Length; ++i)
+    {
+        char ch = line[i];
+        if (ch == '"' || ch == '\\')
+            ++chars;
+        ++chars;
+    }
+    return chars - line.Length;
 }
 
 static int ComputeSizeDifference(string line)
